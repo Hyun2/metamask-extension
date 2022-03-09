086fb945195151142c7e0f592266f41c7cce071a
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-09 10:51:56 +0100

    Update controllers package

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -34,16 +34,15 @@ import {
   CollectiblesController,
   AssetsContractController,
   CollectibleDetectionController,
-} from '@metamask/controllers';
-import SmartTransactionsController from '@metamask/smart-transactions-controller';
-import {
   PermissionController,
   SubjectMetadataController,
   ///: BEGIN:ONLY_INCLUDE_IN(flask)
-  SnapController,
+  RateLimitController,
   ///: END:ONLY_INCLUDE_IN
-} from '@metamask/snap-controllers';
+} from '@metamask/controllers';
+import SmartTransactionsController from '@metamask/smart-transactions-controller';
 ///: BEGIN:ONLY_INCLUDE_IN(flask)
+import { SnapController } from '@metamask/snap-controllers';
 import { IframeExecutionService } from '@metamask/iframe-execution-environment-service';
 ///: END:ONLY_INCLUDE_IN
 
@@ -230,9 +229,15 @@ export default class MetamaskController extends EventEmitter {
       state: initState.TokensController,
     });
 
-    this.assetsContractController = new AssetsContractController({
-      provider: this.provider,
-    });
+    this.assetsContractController = new AssetsContractController(
+      {
+        onPreferencesStateChange: (listener) =>
+          this.preferencesController.store.subscribe(listener),
+      },
+      {
+        provider: this.provider,
+      },
+    );
 
     this.collectiblesController = new CollectiblesController(
       {
